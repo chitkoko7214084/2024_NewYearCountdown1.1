c4d6b1c2dee301d024dc03aa225de736a692ae23
CodeSandbox <support@codesandbox.io>
2023-11-06 19:19:17 +0000

    Sandbox snapshot commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/EXYCl6DB9Q695yc2/scene.splinecode" />
+  );
+}
